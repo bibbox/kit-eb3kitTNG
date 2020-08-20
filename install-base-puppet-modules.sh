9ac6ae581d29ef1e390f562824c2cbddde08c7cf
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-08-20 13:50:19 +0200

    Update install-base-puppet-modules.sh

--- install-base-puppet-modules.sh
+++ install-base-puppet-modules.sh
@@ -13,7 +13,6 @@ echo "##########################################################################
 /opt/puppetlabs/bin/puppet module install --force puppet-archive --version 4.5.0      --modulepath /etc/puppetlabs/code/modules
 /opt/puppetlabs/bin/puppet module install --force puppetlabs-vcsrepo --version 3.1.1  --modulepath /etc/puppetlabs/code/modules
 /opt/puppetlabs/bin/puppet module install --force puppet-alternatives --version 3.0.0 --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppetlabs-docker_platform --version 2.1.0 --modulepath /etc/puppetlabs/code/modules
 /opt/puppetlabs/bin/puppet module install --force puppetlabs-docker --version 3.11.0--modulepath /etc/puppetlabs/code/modules
 /opt/puppetlabs/bin/puppet module install --force puppetlabs-postgresql --version 6.6.0 --modulepath /etc/puppetlabs/code/modules
 /opt/puppetlabs/bin/puppet module install --force tylerwalts-jdk_oracle --version 2.0.0 --modulepath /etc/puppetlabs/code/modules

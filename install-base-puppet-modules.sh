4365bfd6f7b8062af8a36ab0fe2491ed33ee67cf
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-08-20 13:36:01 +0200

    Update install-base-puppet-modules.sh

--- install-base-puppet-modules.sh
+++ install-base-puppet-modules.sh
@@ -5,15 +5,15 @@ echo "##########################################################################
 echo "#                                  PUPPET MODULES                                        #"
 echo "##########################################################################################"
 
-/opt/puppetlabs/bin/puppet module install puppetlabs-stdlib --version 7.5.0  --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppetlabs-apt --version 7.5.0      --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppetlabs-ntp --version 8.3.0      --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppetlabs-firewall --version 2.5.0 --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppetlabs-apache --version 5.5.0  --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppet-archive --version 4.5.0      --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppetlabs-vcsrepo --version 3.1.1  --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppet-alternatives --version 3.0.0 --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppetlabs-docker_platform --version 2.1.0 --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppetlabs-docker --version 3.11.0--modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install puppetlabs-postgresql --version 6.6.0 --modulepath /etc/puppetlabs/code/modules
-/opt/puppetlabs/bin/puppet module install tylerwalts-jdk_oracle --version 2.0.0 --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppetlabs-stdlib --version 7.5.0  --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppetlabs-apt --version 7.5.0      --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppetlabs-ntp --version 8.3.0      --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppetlabs-firewall --version 2.5.0 --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppetlabs-apache --version 5.5.0  --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppet-archive --version 4.5.0      --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppetlabs-vcsrepo --version 3.1.1  --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppet-alternatives --version 3.0.0 --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppetlabs-docker_platform --version 2.1.0 --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppetlabs-docker --version 3.11.0--modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force puppetlabs-postgresql --version 6.6.0 --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install --force tylerwalts-jdk_oracle --version 2.0.0 --modulepath /etc/puppetlabs/code/modules
